85c2d374117931ee36d7ca72354b25d589096cd5
KaterynaSokol <k.sokol2019@gmail.com>
2025-01-22 20:41:16 +0200

    hw3

---
+++ src/controllers/contacts.js
@@ -0,0 +1,76 @@
+import * as contactServices from '../services/contacts.js';
+import createError from 'http-errors';
+
+export const getContactsController = async (req, res) => {
+  const data = await contactServices.getContacts();
+
+  res.json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { id } = req.params;
+  const data = await contactServices.getContactByID(id);
+  if (!data) {
+    throw createError(404, `Contact with id=${id} not found`);
+    // const error = new Error(`Contact with id=${id} not found`);
+    // error.status = 404;
+    // throw error;
+  }
+
+  res.json({
+    status: 200,
+    message: `Succesfully find contact with id=${id}`,
+    data,
+  });
+};
+
+export const addContactController = async (req, res) => {
+  const data = await contactServices.addContact(req.body);
+  res.status(201).json({
+    status: 201,
+    message: 'Succesfully add contact',
+    data,
+  });
+};
+
+export const upsertContactController = async (req, res) => {
+  const { id } = req.params;
+  const { isNew, data } = await contactServices.updateContact(id, req.body, {
+    upsert: true,
+  });
+  const status = isNew ? 201 : 200;
+  res.status(status).json({
+    status,
+    message: 'Successfully upsert contact',
+    data,
+  });
+};
+
+export const patchContactController = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactServices.updateContact(id, req.body);
+
+  if (!result) {
+    throw createError(404, `Contact with id=${id} not found`);
+  }
+
+  res.json({
+    status: 200,
+    message: 'Successfully update contact',
+    data: result.data,
+  });
+};
+
+export const deleteContactController = async (req, res) => {
+  const { id } = req.params;
+  const data = await contactServices.deleteContact({ _id: id });
+  if (!data) {
+    throw createError(404, `Contact with id=${id} not found`);
+  }
+
+  res.status(204).send();
+};
--- src/server.js
+++ src/server.js
@@ -1,50 +1,21 @@
 import express from 'express';
 import cors from 'cors';
-import * as contactServices from './services/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
+import contactsRouter from './routers/contacts.js';
+import { logger } from './middlewares/logger.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export const setupServer = () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
+  app.use(logger);
 
-  app.get('/contacts', async (req, res) => {
-    const data = await contactServices.getContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
-    });
-  });
+  app.use('/contacts', contactsRouter);
 
-  app.get('/contacts/:id', async (req, res) => {
-    const { id } = req.params;
-    const data = await contactServices.getContactByID(id);
-    if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with id=${id} not found`,
-      });
-    }
-    res.json({
-      status: 200,
-      message: `Succesfully find contact with id=${id}`,
-      data,
-    });
-  });
-
-  app.get((req, res) => {
-    res.status(404).json({
-      message: `${req.url} not found`,
-    });
-  });
-
-  app.use((error, res, req, next) => {
-    res.status(500).json({
-      message: 'Not found',
-      error: error.message,
-    });
-  });
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
   const port = Number(getEnvVar('PORT', 3000));
 
